921f8374af5e7456f2c617445b49b28e4a43e5b2
Julia Bush <j.elizabethbush@gmail.com>
2025-12-07 20:24:06 +0100

    add: skeleton for setUser function and imports for file system, os and path

--- config.ts
+++ config.ts
@@ -1,4 +1,12 @@
+import fs from "fs";
+import os from "os";
+import path from "path";
+
 export type Config = {
   dbUrl: string;
   currentUserName: string;
 };
+
+export function setUser(currentUserName: string): Object {
+  currentUserName = new Config[currentUserName]();
+}
