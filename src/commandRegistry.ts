f903112e8a60ac5bc09c7f276965f618485aebea
Julia Bush <j.elizabethbush@gmail.com>
2026-01-01 00:36:48 +0100

    add: filling out skeleton of function

--- src/commandRegistry.ts
+++ src/commandRegistry.ts
@@ -1,6 +1,12 @@
-type CommandRegistry = {
-  cmdName: string;
-  cmdHandlerValues: string;
-};
+import { CommandHandler } from "./commandHandler";
 
-function registerCommand(registry);
+export type CommandsRegistry = Record<string, CommandHandler>;
+// mapping names to functions, key is command name, value is handler
+
+export function registerCommand(
+  registry: CommandsRegistry,
+  cmdName: string,
+  handler: CommandHandler
+): void {
+  registry[cmdName] = handler;
+}
