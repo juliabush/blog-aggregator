aee795b57e2120e83f3cbb41ca7b86edf95223ae
Julia Bush <j.elizabethbush@gmail.com>
2026-01-01 18:54:01 +0100

    add: adjustements to login function and creating a handlerRegister user function for cli register user

--- src/commandHandler.ts
+++ src/commandHandler.ts
@@ -1,4 +1,5 @@
 import { setUser } from "./config";
+import { createUser, fetchUser } from "./db/queries/users";
 
 export type CommandHandler = (
   cmdName: string,
@@ -10,6 +11,26 @@ export async function handlerLogin(cmdName: string, ...args: string[]) {
     throw new Error("Commands array is empty");
   }
   const username = args[0];
+  let checkUsername = await fetchUser(username);
+  if (!checkUsername) {
+    throw new Error("Cannot login with nonexistant account");
+  }
   setUser(username);
   console.log("User has been set");
+}
+
+export async function handlerRegisterUser(cmdName: string, ...args: string[]) {
+  if (args.length === 0) {
+    throw new Error("You must pass a name as argument");
+  }
+  const potencialUser = args[0];
+
+  //   is this a valid check
+  let checkUser = await fetchUser(potencialUser);
+  if (checkUser) {
+    throw new Error("User already exists");
+  }
+  await createUser(potencialUser);
+  setUser(potencialUser);
+  console.log("New user has been registered");
 }
