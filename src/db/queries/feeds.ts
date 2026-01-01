070b9bc5f6f1e0a162c3e71e5a881f5ed30d76c6
Julia Bush <j.elizabethbush@gmail.com>
2026-01-02 00:02:41 +0100

    add: printFeed function and type of Feed inferring from table

--- src/db/queries/feeds.ts
+++ src/db/queries/feeds.ts
@@ -1,5 +1,6 @@
 import { db } from "..";
 import { feeds } from "../schema";
+import type { user } from "../queries/users";
 
 export async function createFeed(name: string, url: string, user_id: string) {
   const [result] = await db
@@ -7,4 +8,11 @@ export async function createFeed(name: string, url: string, user_id: string) {
     .values({ name: name, url: url, user_id: user_id })
     .returning();
   return result;
+}
+
+export type Feed = typeof feeds.$inferSelect;
+
+export async function printFeed(Feed: Feed, User: User) {
+  console.log(Feed);
+  console.log(User);
 }
