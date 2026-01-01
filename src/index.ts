e69d936dd5a67f1f61097ff80956b3cba5c1b51c
Julia Bush <j.elizabethbush@gmail.com>
2026-01-01 16:02:37 +0100

    add: functions and logic necessary for accepting command name in cli and setting  user

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,23 @@
 import { readConfig, setUser } from "./config";
+import { handlerLogin } from "./commandHandler";
+import type { CommandsRegistry } from "./commandRegistry";
+import { runCommand } from "./commandRegistry";
+
+import { exit } from "node:process";
 
 function main() {
   setUser("Julia");
-  const cfg = readConfig();
-  console.log(cfg);
+  const registry: CommandsRegistry = {
+    login: handlerLogin,
+  };
+  let sliced_cli = process.argv.slice(2);
+  if (sliced_cli.length === 0) {
+    console.error("More arguments ae required");
+    process.exit(1);
+  }
+  const cmdName = sliced_cli[0];
+  const argArray = sliced_cli.slice(1);
+  runCommand(registry, cmdName, ...argArray);
 }
 
 main();
