b4476c7225ea2af670abc0cabd84fb3da67287b8
Julia Bush <j.elizabethbush@gmail.com>
2026-01-02 16:50:50 +0100

    update & add: returning from createFeedFollow and selecting fields from table as well as helper getFeedByUrl function

--- src/db/queries/feed_follow.ts
+++ src/db/queries/feed_follow.ts
@@ -8,9 +8,20 @@ export async function createFeedFollow(user_id: string, feed_id: string) {
     .values({ user_id, feed_id })
     .returning();
   const joinedResult = await db
-    .select({})
+    .select({
+      feedName: feeds.name,
+      userName: users.name,
+    })
     .from(feed_follow)
     .innerJoin(feeds, eq(feed_follow.feed_id, feeds.id))
     .innerJoin(users, eq(feed_follow.user_id, users.id))
     .where(eq(feed_follow.id, newFeedFollow.id));
+  return joinedResult[0];
 }
+
+export async function getFeedByUrl(url: string) {
+  const [result] = await db.select().from(feeds).where(eq(feeds.url, url));
+  return result;
+}
+
+export async function getFeedFollowersForUser() {}
