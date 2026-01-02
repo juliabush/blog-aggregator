6a2741e05b835a4c34feaba110cf8792626fe13d
Julia Bush <j.elizabethbush@gmail.com>
2026-01-02 14:08:05 +0100

    update: minor alterations to the function

--- src/db/queries/feed_follow.ts
+++ src/db/queries/feed_follow.ts
@@ -1,15 +1,16 @@
 import { db } from "..";
-import { feed_follow } from "../schema";
+import { feed_follow, feeds, users } from "../schema";
+import { eq } from "drizzle-orm";
 
-export async function createFeedFollow() {
+export async function createFeedFollow(user_id: string, feed_id: string) {
   const [newFeedFollow] = await db
     .insert(feed_follow)
-    .select({ user_id, feed_id })
+    .values({ user_id, feed_id })
     .returning();
   const joinedResult = await db
     .select({})
     .from(feed_follow)
     .innerJoin(feeds, eq(feed_follow.feed_id, feeds.id))
     .innerJoin(users, eq(feed_follow.user_id, users.id))
-    .where(eq(feed_follow.user_id, users.id));
+    .where(eq(feed_follow.id, newFeedFollow.id));
 }
