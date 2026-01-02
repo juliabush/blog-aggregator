5ef92fc43a7843ed122514631cbfa405db767508
Julia Bush <j.elizabethbush@gmail.com>
2026-01-02 19:59:27 +0100

    add: defining the middlewareLoggedIn function

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,5 @@
+import { readConfig } from "./config";
+import { fetchUser } from "./db/queries/users";
 import type { User } from "./db/queries/users";
 import type { CommandHandler } from "./commandHandler";
 
@@ -8,3 +10,22 @@ type UserCommandHandler = (
 ) => Promise<void>;
 
 type middlewareLoggedIn = (handler: UserCommandHandler) => CommandHandler;
+
+export function middlewareLoggedIn(
+  handler: (cmdName: string, user: User, ...args: string[]) => Promise<void>
+): CommandHandler {
+  return async (cmdName: string, ...args: string[]) => {
+    const cfg = readConfig();
+
+    if (!cfg.currentUserName) {
+      throw new Error("No user is currently logged in.");
+    }
+
+    const user = await fetchUser(cfg.currentUserName);
+    if (!user) {
+      throw new Error(`User ${cfg.currentUserName} not found`);
+    }
+
+    await handler(cmdName, user, ...args);
+  };
+}
