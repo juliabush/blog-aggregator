a283a37a242f53aae656a20a66b54d47cd9de712
Julia Bush <j.elizabethbush@gmail.com>
2026-01-02 13:37:12 +0100

    add: new table in schema feed_follow

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -4,6 +4,7 @@ import {
   uuid,
   text,
   foreignKey,
+  unique,
 } from "drizzle-orm/pg-core";
 
 export const users = pgTable("users", {
@@ -34,8 +35,35 @@ export const feeds = pgTable(
       userFk: foreignKey({
         columns: [table.user_id],
         foreignColumns: [users.id],
-        // onDelete: "cascade"
-      }),
+      }).onDelete("cascade"),
+    };
+  }
+);
+
+export const feed_follow = pgTable(
+  "feed_follows",
+  {
+    id: uuid("id").primaryKey().defaultRandom().notNull(),
+    createdAt: timestamp("created_at").notNull().defaultNow(),
+    updatedAt: timestamp("updated_at")
+      .notNull()
+      .defaultNow()
+      .$onUpdate(() => new Date()),
+    user_id: uuid("user_id").notNull(),
+    feed_id: uuid("feed_id").notNull(),
+  },
+  (table) => {
+    return {
+      feedFk: foreignKey({
+        columns: [table.feed_id],
+        foreignColumns: [feeds.id],
+      }).onDelete("cascade"),
+      userFk: foreignKey({
+        columns: [table.user_id],
+        foreignColumns: [users.id],
+      }).onDelete("cascade"),
+
+      uniquePair: unique().on(table.user_id, table.feed_id),
     };
   }
 );
